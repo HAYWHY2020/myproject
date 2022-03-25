b8e292a116dcba116a6c7c8da8bdcfa6862954c3
haywhy <adislaw2014@gmail.com>
2022-03-25 15:13:40 +0000

    models

--- models/roomsModel.js
+++ models/roomsModel.js
@@ -1,4 +1,5 @@
 var pool = require('./connection.js')
+var beats = { rock: 'scissors', scissors: 'paper', paper: 'rock' };
 
 module.exports.getAllRooms = async function() {
   try {
@@ -11,13 +12,11 @@ module.exports.getAllRooms = async function() {
     console.log(err);
     return { status: 500, result: err};
   }
-} 
-
-
+}
 module.exports.getRoomById = async function (id) {
   try {
     let sql = `Select roo_id,roo_name, crd_name as roo_topcard 
-    from room, card where roo_topcard_id = crd_id`
+               from room, card where roo_topcard_id = crd_id and roo_id = $1`;
     let result = await pool.query(sql, [id]);
     if (result.rows.length > 0) {
       let room = result.rows[0];
@@ -30,51 +29,56 @@ module.exports.getRoomById = async function (id) {
     return { status: 500, result: err };
   }
 }
-
-var beats = { rock: 'scissors', scissors: 'paper', paper: 'rock' };
 module.exports.play = async function (id, value) {
   try {
-    if (!beats[value]) {
-      return { status: 400, result: { msg: "Card value is not valid (rock,paper,scissors)" } };
+    if (!parseInt(id)) {
+      return { status: 400, result: { msg: "Room id must be a number" } };      
     }
     let sql = `select * from room, card, cardwcard
-    where room.roo_id = $1 and
-    room.roo_topcard_id = cardwcard.cwc_clooses_id and
-    card.crd_id = cardwcard.cwc_cwins_id and
-    card.crd_name ILIKE $2;`
-    
-    let room = result.rows[0];
-    if (beats[value] != room.roo_topcard.toLowerCase()) {
-      return {
-        status: 200,
-        result: {
-          victory: false,
-          msg: "You Lost! That card does not beat the top card.",
-          current_topcard: room.roo_topcard
-        }
-      };
+                where room.roo_id = $1 and
+                room.roo_topcard_id = cardwcard.cwc_clooses_id and
+                card.crd_id = cardwcard.cwc_cwins_id and
+                card.crd_name ILIKE $2;`
+    let result = await pool.query(sql, [id,value]);
+    if (result.rows.length == 0) {
+      let sqlr = `select * from room, card where room.roo_id = $1 
+                  and room.roo_topcard_id = card.crd_id`;
+      let resultr = await pool.query(sqlr, [id]);
+      let room = resultr.rows[0];
+      if (!room) {
+        return { status: 404, result: { msg: "No room with that id" } };
+      } else {
+        return {
+          status: 200,
+          result: {
+            victory: false,
+            msg: "You Lost! That card does not beat the top card.",
+            current_topcard: room.crd_name         
+          }
+        };
+      }
     }
-    let sql2 = "UPDATE room SET roo_topcard = $1 WHERE roo_id = $2";
-    let result2 = await pool.query(sql2, [value, id]);
+    let card_id =  result.rows[0].crd_id;
+    let card_name = result.rows[0].crd_name;
+    let sql2 = "UPDATE room SET roo_topcard_id = $1 WHERE roo_id = $2";
+    let result2 = await pool.query(sql2, [  card_id, id  ]);
     if (result2.rowCount == 0) {
       return { status: 500, 
-                result: { msg: "Not able to update. Many possible reasons (ex: room was deleted during play)" } };
+               result: { msg: "Not able to update. Many possible reasons (ex: room was deleted during play)" } };
     }
     return {
       status: 200,
       result: {
         victory: true,
         msg: "You Won!",
-        current_topcard: value
+        current_topcard: card_name
       }
     };
-
   } catch (err) {
     console.log(err);
     return { status: 500, result: err };
   }
-}
-
+}    
 module.exports.getRoomByNameOrTopCard = async function (parameters) {
   try {
     if (!parameters.name && !parameters.topcard) {
@@ -83,7 +87,7 @@ module.exports.getRoomByNameOrTopCard = async function (parameters) {
     let nparam = 1;
     let values = [];
     let sql = `Select roo_id,roo_name, crd_name as roo_topcard 
-    from room, card where roo_topcard_id = crd_id`
+                from room, card where roo_topcard_id = crd_id`
 
     if (parameters.name) {
       sql += ` and roo_name ILIKE $${nparam}`;
@@ -91,11 +95,12 @@ module.exports.getRoomByNameOrTopCard = async function (parameters) {
       nparam++;
     }
     if (parameters.topcard) {
-      if (parameters.name) sql+=" AND"
-      sql += ` roo_topcard LIKE $${nparam}`;
+      sql += ` and crd_name ILIKE $${nparam}`;
       values.push(parameters.topcard);
       nparam++;
     }
+    console.log(sql)
+    console.log(values)
     let result = await pool.query(sql, values);
     let rooms = result.rows;
     return { status: 200, result: rooms };
